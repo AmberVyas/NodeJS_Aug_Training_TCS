b5b3fa7e8c3515a003f5ec22720ac04abe148353
Amber <amber081992@gmail.com>
2022-08-15 00:41:17 +0530

    Upsated with axios

--- router/users.mjs
+++ router/users.mjs
@@ -1,4 +1,35 @@
 import express from "express";
-
+import axios from "axios"
+//import https from "https";
 const router = express.Router();
 
+router.get("/",async (req,res)=>{
+    try {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+        res.json(response.data)
+
+    } catch (error) {
+        res.status(500).send("internal server error");
+    }
+    
+
+})
+
+// router.get("/",(req,res)=>{
+//     https.get({
+//         hostname : "jsonplaceholder.typicode.com",
+//         path : "/users",
+//         protocol : "https:"
+//         },(response)=>{
+//             let data = "";
+//             response.on("data",(chunk)=>{
+//                 data = data + chunk;
+//             })
+//             response.on("close",()=>{
+//                 res.json(data);
+//             })
+//         }
+//         )
+// })
+
+export default router;
